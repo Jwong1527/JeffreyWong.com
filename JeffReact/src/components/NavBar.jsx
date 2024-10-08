dd7898e1e9d3e3dbebcb84d0514f43fba0c34583
Jwong1527 <112716191+Jwong1527@users.noreply.github.com>
2024-10-08 18:27:45 -0400

    Initial commit

---
+++ JeffReact/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return <nav>
+    <a href= "/" ></a>
+    <div>
+        <ul>
+            <li><a> href="#about"</a></li>
+        </ul>
+    </div>
+  </nav>
+}
+
+export default NavBar
